8dbab6dc9ff7f26df9c887df4875ac6599089cde
michaelwallace30 <michael.wallace30@gmail.com>
2016-02-18 07:51:16 -0800

    Linux library and tested.

---
+++ build/linux/CMakeFiles/NGCP_COM.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bender/Documents/Comms/protonet/src/cond.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/cond.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/datalink.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/datalink.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/AmyStuff.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/AmyStuff.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/Enter.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/Enter.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/Exit.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/Exit.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/PayloadBayCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/PayloadBayCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/PayloadBayModeCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/PayloadBayModeCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/Ping.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/Ping.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/Pong.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/Pong.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/TargetDesignationCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/TargetDesignationCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/UGVBatteryStatus.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/UGVBatteryStatus.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/UGVJoystick.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/UGVJoystick.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleAttitude.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleAttitude.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleAuthorizationReply.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleAuthorizationReply.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleAuthorizationRequest.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleAuthorizationRequest.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleBodySensedState.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleBodySensedState.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleGlobalPosition.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleGlobalPosition.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleGroundRelativeState.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleGroundRelativeState.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleIdentification.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleIdentification.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleInertialState.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleInertialState.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleModeCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleModeCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleSystemStatus.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleSystemStatus.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleTelemetryCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleTelemetryCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleTerminationCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleTerminationCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/message/VehicleWaypointCommand.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/message/VehicleWaypointCommand.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/mutex.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/mutex.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/pqueue.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/pqueue.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/protonet.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/protonet.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/protonet_marshal.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/protonet_marshal.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/protonet_message.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/protonet_message.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/protonet_parser.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/protonet_parser.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/protonet_protocol.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/protonet_protocol.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/serial.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/serial.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/thread.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/thread.cpp.o"
+  "/home/bender/Documents/Comms/protonet/src/udp.cpp" "/home/bender/Documents/Comms/build/linux/CMakeFiles/NGCP_COM.dir/protonet/src/udp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../protonet/src"
+  "../../protonet/src/message"
+  "../../protonet/include"
+  "../../protonet/include/message"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
