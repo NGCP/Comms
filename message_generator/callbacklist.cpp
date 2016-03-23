f2570153783d9d74887fb930ccdd2dd0a207c21a
michaelwallace30 <michael.wallace30@gmail.com>
2016-03-23 10:39:57 -0700

    Added call back list generator to generate empty functions of all possible message types.

---
+++ message_generator/callbacklist.cpp
@@ -0,0 +1,115 @@
+void* Enter(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Exit(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Ping(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Pong(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Identification(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Amy_Stuff(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Authorization_Request(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Authorization_Reply(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Mode_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Termination_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Telemetry_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Waypoint_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_System_Status(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Inertial_State(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Global_Position(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Body_Sensed_State(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Attitude(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Vehicle_Ground_Relative_State(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Payload_Bay_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Payload_Bay_Mode_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* Target_Designation_Command(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* UGV_Joystick(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
+void* UGV_Battery_Status(int8_t link_id, com_header_t, header, ping_t ping, comnet::node* node)
+{
+
+}
+
