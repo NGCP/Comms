482da40cacbe00b83af779ffb9da0bef9cd3d97e
jessieeisenberg <jessieeisenberg@AlexDebian.JessieEisenberg.edu>
2016-05-27 15:52:30 -0700

    Add CMakeLists.txt

---
+++ Spoofing/UAVMessage/build/CMakeFiles/UAVtest.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jessieeisenberg/Documents/GitHub/Comms/Spoofing/UAVMessage/UAVMessage/fromGCS.cpp" "/home/jessieeisenberg/Documents/GitHub/Comms/Spoofing/UAVMessage/build/CMakeFiles/UAVtest.dir/UAVMessage/fromGCS.cpp.o"
+  "/home/jessieeisenberg/Documents/GitHub/Comms/Spoofing/UAVMessage/UAVMessage/main.cpp" "/home/jessieeisenberg/Documents/GitHub/Comms/Spoofing/UAVMessage/build/CMakeFiles/UAVtest.dir/UAVMessage/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../../comnet/include"
+  "../../../comnet/include/message"
+  "../../../cryptopp563/include"
+  "../../../libxbee3-3.0.11/include"
+  "../../../Spoofing/UAVMessage/UAVMessage"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Spoofing/UAVMessage/build/CMakeFiles/UAVtest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UAVtest.dir/UAVMessage/fromGCS.cpp.o"
+  "CMakeFiles/UAVtest.dir/UAVMessage/main.cpp.o"
+  "UAVtest.pdb"
+  "UAVtest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UAVtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
